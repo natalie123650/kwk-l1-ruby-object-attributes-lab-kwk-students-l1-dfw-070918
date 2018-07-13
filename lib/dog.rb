1d47b69e0f96031834ab654d82b100bfebc3bcfe
Natalie Emanuel <natalieazana@gmail.com>
2018-07-13 19:03:55 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,19 @@
 # dog.rb
 
 class Dog 
-  attr_reader :name, :breed 
-  attr_writer :age
   def initialize(name, breed, age)
     @name = name
     @breed = breed 
     @age = age 
   end 
-end
+  
+  def name
+    @name
+  end
+  
+  def name=(name)
+    @name=name
+  end 
 
 vlad = Dog.new("Bowie", "Mini Poodle", "Almost 3")
 puts vlad.name 
