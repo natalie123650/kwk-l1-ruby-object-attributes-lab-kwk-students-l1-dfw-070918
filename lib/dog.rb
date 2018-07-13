137ead357b35ea5a1ecd806af8e6b926d25bc8fb
Natalie Emanuel <natalieazana@gmail.com>
2018-07-13 18:54:16 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
 # dog.rb
+
+class Dog 
+  attr_reader :name, :breed 
+  attr_writer :age
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed 
+    @age = age 
+  end 
+end
+
+vlad = Dog.new("Bowie", "Mini Poodle", "Almost 3")
+puts vlad.name 
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,15 @@
 # person.rb
+class Person 
+  attr_reader :name, :race
+  attr_writer :height, :age, :job
+  def initialize(name, race, height, age, job)
+    @name = name
+    @height = height
+    @age = age
+    @race = race
+    @job = job
+  end 
+end 
+
+natalie = Person.new("Natalie", "Black", "68 inches", "16", "actress")
+puts natalie.height = "68 inches"
